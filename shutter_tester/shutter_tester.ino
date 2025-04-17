15bc6bcc94f6121357f703cdf2051d02662f5eed
Zloj-blinchik <blinov_i_v@mail.ru>
2025-04-17 07:53:59 +0300

    используем только прерывания от ацп. во время срабатывания ацп вычисляем разницу в отсчётах таймера между фронтами импульса

--- shutter_tester/shutter_tester.ino
+++ shutter_tester/shutter_tester.ino
@@ -16,17 +16,18 @@
 #define BUFF 4
 volatile uint8_t val[BUFF];
 volatile uint8_t value;
-volatile uint16_t sum = 0;
 volatile uint16_t curr_calib_value = 0;
 volatile uint16_t prev_calib_value = 0;
 volatile uint16_t curr_top = 0;
 volatile uint8_t flag = 0; //флаг активности импульса
 volatile uint8_t flag_output = 0; //флаг готовности результата 
 volatile uint16_t trashold; //граница активного уровня импульса
-volatile uint16_t counter = 0;
+volatile uint16_t counter_start = 0;
 
 volatile uint8_t flag_tcv = 0;
 volatile uint8_t flag_adcc = 0;
+
+volatile uint16_t output;
 
 void setup()
 {
@@ -38,12 +39,9 @@ void setup()
     TCCR1A = 0;
     TCCR1B = 0;
 
-    OCR1A = CTC_VAL;        // установка регистра совпадения
-    TCCR1B |= (1 << WGM12); // включение в CTC режим
     TCCR1B |= (1 << CS10);  // Установить CS10 бит так, чтобы таймер работал при тактовой частоте:
 
-    TIMSK1 |= (1 << OCIE1A);  // включение прерываний по совпадению
-    
+   
   //Инициализация ADC A0
   pinMode(A0, INPUT);
   
@@ -57,8 +55,8 @@ void setup()
   ADMUX |= (0 & 0x07);    // Выбираем пин A0 для преобразования
   
   // Устанавливаем предделитель - 8 (ADPS[2:0]=011)
-  ADCSRA &= ~(1 << ADPS2);
-  ADCSRA |= ((1 << ADPS1) | (1 << ADPS0));
+  ADCSRA |= (1 << ADPS2);
+  ADCSRA &= ~((1 << ADPS1) | (1 << ADPS0));
 
   ADCSRA |= (1 << ADATE); // Включаем автоматическое преобразование
   ADCSRA |= (1 << ADIE);  // Разрешаем прерывания по завершении преобразования
@@ -66,61 +64,69 @@ void setup()
   ADCSRA |= (1 << ADSC);  // Запускаем преобразование
   
     
-    sei();                  // включить глобальные прерывания
+    
 
     Serial.begin(115200);
 
     Serial.println("Initialisation done.");
+
+    sei();                  // включить глобальные прерывания */
 }
 
 void loop(){
-  Serial.print(value);
-  Serial.print(' ');
-  Serial.print(ADCH);
-  Serial.println(' ');
-  if(flag_output == 1){
-    Serial.println(counter);
-    flag_output = 0;
+  while(1){
+    //Serial.print(TCNT1);
+    //Serial.print(' ');
+    Serial.print(ADCH);
+    Serial.println(' ');
+    /*if(flag_output == 1){
+      Serial.println(output);
+      flag_output = 0;
+    }*/
   }
-
 }
 
-ISR(TIMER1_COMPA_vect)
-{
-  flag_tcv = 1;
-     curr_calib_value = sum >> 2;
-     
-     if(flag == 0){
-       if(curr_calib_value > 4*prev_calib_value){
-          flag = 1;
-          counter = 1;
-          trashold = TRASHOLD_PERCENT * curr_calib_value; 
-       }
-     }
 
-     if(flag == 1){
-       if(curr_calib_value < trashold){
-          flag = 0;
-          flag_output = 1;
-       }else{
-          counter++;
-       }
-     }
-     
-     prev_calib_value = curr_calib_value;
-
-     
-}
 
 ISR(ADC_vect){
+  
+  cli();
+  
   value = ADCH;  // Считываем только значимые 8 бит - значение из диапазона 0...255
-  //cli();
-  for(int8_t i = 1; i < BUFF; i++){
-    val[i-1] = val[i];
+  int16_t sum = 0;
+  int8_t i;
+  for(i = 1; i < BUFF; i++){
     sum+=val[i];
+    val[i-1] = val[i];
+  }
+  val[BUFF-1] = value;
+  sum+=value;
+  
+  curr_calib_value = sum >> 2;
+
+  if(flag == 0){
+     if(curr_calib_value > (prev_calib_value<<2)){
+        flag = 1;
+        counter_start = TCNT1;
+        trashold = (curr_calib_value>>1)+(curr_calib_value>>2); 
+     }
   }
 
-  val[BUFF-1] = value;
-  sum+= value;
-  //sei();
+  if(flag == 1){
+     if(curr_calib_value < trashold){
+        flag = 0;
+        flag_output = 1;
+        if(TCNT1<counter_start){
+          (0xFFFF - counter_start)+TCNT1;
+        }else{
+          output = TCNT1 - counter_start;
+        }
+     }
+    
+  }
+
+  prev_calib_value = curr_calib_value;
+  
+  sei();
+  
 }
